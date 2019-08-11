2ac6a7d388178e063b12814000807ff1bc7e9a92
Christian Serrano <nano561@icloud.com>
2019-08-11 13:01:05 -0400

    Reformatted Code & Corrected spelling

--- Section 4/app.js
+++ Section 4/app.js
@@ -2,15 +2,15 @@
 GAME RULES:
 
 - The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- In each turn, a player rolls a dice as many times as he wishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
 var scores, roundScores, activePlayer;
 
-scores = [0,0]; //score array for player 1 and 2
+scores = [0, 0]; //score array for player 1 and 2
 roundScores = 0;
 activePlayer = 1; //player 1 = 0, player 2 = 1, this matches with array "score"
 
@@ -27,7 +27,7 @@ document.getElementById('current-0').textContent = '0';
 document.querySelector('.btn-roll').addEventListener('click', function () { //'click' = event, btn = call back function
 
     // 1. Random #
-    var dice = Math.floor(Math.random()*6) + 1;
+    var dice = Math.floor(Math.random() * 6) + 1;
 
     // 2. Display the result
     var diceDOM = document.querySelector('.dice');
@@ -36,17 +36,9 @@ document.querySelector('.btn-roll').addEventListener('click', function () { //'c
 
 
     // 3. Update the round score IF the rolled # is NOT a #1
-    
-    
-    
+
+
 });
-
-
-
-
-
-
-
 
 
 // # = id
