3ab724c76cf3ea7ecccd3f3d48b73931705f7194
Christian Serrano <nano561@icloud.com>
2019-08-11 21:46:59 -0400

    Updated Scores and Changing the Active Player
    Updated: Readme.md

--- Section 4/app.js
+++ Section 4/app.js
@@ -1,5 +1,5 @@
 /*
-GAME RULES:
+GAME RULES: The Pig Game
 
 - The game has 2 players, playing in rounds
 - In each turn, a player rolls a dice as many times as he wishes. Each result get added to his ROUND score
@@ -8,25 +8,28 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
 var scores, roundScores, activePlayer;
 
 scores = [0, 0]; //score array for player 1 and 2
-roundScores = 0;
-activePlayer = 1; //player 1 = 0, player 2 = 1, this matches with array "score"
+roundScores = 0; //current round score, this will be added to the total of the player score
+activePlayer = 0; //player 1 = 0, player 2 = 1, this matches with array "score"
 
 
-// . = class
-document.querySelector('.dice').style.display = 'none'; //hides class = dice img
+// . = class || # = id || hides dice img (below code)
+document.querySelector('.dice').style.display = 'none';
 
+// This will set all the below values to 0 or clear the board/score
 document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
 document.getElementById('current-0').textContent = '0';
-document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
-//you can replace the call back function, with an actual function, this would be considered "anonymous function" and cannot be reused.
-document.querySelector('.btn-roll').addEventListener('click', function () { //'click' = event, btn = call back function
+//you can replace the call back function, with an actual function, this would be considered "anonymous function" and cannot be called.
+//'click' = event, btn = call back function
+document.querySelector('.btn-roll').addEventListener('click', function () {
 
-    // 1. Random #
+    // 1. Random the dice between 1 - 6
     var dice = Math.floor(Math.random() * 6) + 1;
 
     // 2. Display the result
@@ -36,14 +39,41 @@ document.querySelector('.btn-roll').addEventListener('click', function () { //'c
 
 
     // 3. Update the round score IF the rolled # is NOT a #1
+    if (dice !== 1 ) {
+        // Add Score
+        roundScores += dice;
+        //Display current score
+        document.querySelector('#current-' + activePlayer).textContent = roundScores;
+    } else {
+        //Next Player turn
+        // this is a simpler if statement || : = else || this is called tannery operator
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
 
+        // This will clear the board without clearing total score
+        roundScores = 0;
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        // This will toggle the active class from play 1 & 2
+        //toggle = if "active is in the class, remove active, else add active
+        //you could replace toggle with add and remove, but toggle makes life easier :)
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        document.querySelector('.dice').style.display = 'none'
+    }
 
 });
+/*  Section 4 Lecture 5 teaching code
 
+//(Setter) gives the current player the current value of dice roll
+ = dice;
 
-// # = id
-//document.querySelector('#current-' + activePlayer).textContent = dice; //(Setter) gives the current player the current value of dice roll
-//document.querySelector('#current-'+ activePlayer).innerHTML = '<em>' + dice + '</em>'; //same as above + adds it as a html code and emphasis it. instead of only text manipulation.
-//var x = document.querySelector('#score-0').textContent; //(Getter) only to read the content and store into variable.
-//console.log(x);
+//same as above + adds it as a html code and emphasis it. instead of only text manipulation.
+document.querySelector('#current-'+ activePlayer).innerHTML = '<em>' + dice + '</em>';
 
+//(Getter) only to read the content and store into variable.
+var x = document.querySelector('#score-0').textContent;
+console.log(x);
+
+*/
