589bb61d7a7984668edd5acb93bd7dfe54adc249
Christian Serrano <nano561@icloud.com>
2019-08-11 23:33:29 -0400

    Added coding challenge text
    Updated: score value of 10 (testing score) to 100 (pig game actual score to win)

--- Section 4/app.js
+++ Section 4/app.js
@@ -9,7 +9,16 @@ GAME RULES: The Pig Game
 
 */
 
-var scores, roundScores, activePlayer, gamePlaying;
+/*
+YOUR 3 CHALLENGES
+Change the game to follow these rules:
+
+1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
+2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
+3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
+*/
+
+var scores, roundScores, activePlayer, gamePlaying, lastDice;
 
 // clear the board
 init();
@@ -52,7 +61,7 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
         //check if player won the game
-        if (scores[activePlayer] >= 10) {
+        if (scores[activePlayer] >= 100) {
 
             //Tells the users who won
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
