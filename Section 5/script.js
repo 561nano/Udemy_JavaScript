be1a9405644138183bb50d3132c6016e7c1b866b
Christian Serrano <nano561@icloud.com>
2019-08-15 17:50:11 -0400

    Coding challenge part 1 complete

--- Section 5/script.js
+++ Section 5/script.js
@@ -14,7 +14,6 @@ var Person = function (name, yearOfBirth, job) {
     this.job = job;
 };
 
-// TODO research inheritance
 // prototype = inheritance in O.O.P
 Person.prototype.calculateAge = function () {
     console.log(2019 - this.yearOfBirth);
@@ -347,28 +346,76 @@ console.log(ages);
 
 1. Build a function constructor called Question to describe a question. A question should include:
 a) question itself
-b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
+b) the answers from which the player can choose the correct one
+(choose an adequate data structure here, array, object, etc.)
 c) correct answer (I would use a number for this)
 
 2. Create a couple of questions using the constructor
 
 3. Store them all inside an array
 
-4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
+4. Select one random question and log it on the console, together with the possible answers
+(each question should have a number) (Hint: write a method for the Question objects for this task).
 
-5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
+5. Use the 'prompt' function to ask the user for the correct answer.
+The user should input the number of the correct answer such as you displayed it on Task 4.
 
-6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
+6. Check if the answer is correct and print to the console whether the answer is correct ot nor
+(Hint: write another method for this).
 
-7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
-
+7. Suppose this code would be a plugin for other programmers to use in their code.
+So make sure that all your code is private and doesn't interfere with the other programmers code
+(Hint: we learned a special technique to do exactly that). */
+/* Expert Level
 ---------------------------------------- Expert level ----------------------------------------
 
-8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+8. After you display the result, display the next random question, so that the game never ends
+(Hint: write a function for this and call it right after displaying the result)
 
-9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if
+the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
 
-10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+10. Track the user's score to make the game more fun! So each time an answer is correct,
+add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to,
+just do this with the tools you feel more comfortable at this point).
 
 11. Display the score in the console. Use yet another method for this.
 */
+
+(function () {
+    function Question (question, playerChoices, correctAnswer) {
+        this.question = question;
+        this.playerChoices = playerChoices;
+        this.correctAnswer = correctAnswer;
+    }
+
+    Question.prototype.displayQuestions = function() {
+        console.log(this.question);
+
+        for (var i = 0; i<this.playerChoices.length; i++) {
+            console.log(i + ': ' + this.playerChoices[i]);
+        }
+    };
+
+    Question.prototype.checkAnswer = function (answer) {
+        if (answer === this.correctAnswer) {
+            console.log('Correct')
+        } else {
+            console.log('Wrong')
+        }
+    };
+
+    var qt0 = new Question('Where is the programmer from?', ['Florida', 'California', 'Puerto Rico'], 2);
+    var qt1 = new Question('Where is the programmer now?', ['Florida', 'California', 'Puerto Rico'], 0);
+    var qt2 = new Question('Where is the programmer grandfather?', ['California', 'Puerto Rico'], 0);
+
+    var qtArray = [qt0, qt1, qt2];
+    var randomQuestion = Math.floor(Math.random() * qtArray.length);
+
+    qtArray[randomQuestion].displayQuestions();
+
+    var playerAnswer = parseInt(prompt("This is a test"));
+
+    qtArray[randomQuestion].checkAnswer(playerAnswer);
+})();
+
