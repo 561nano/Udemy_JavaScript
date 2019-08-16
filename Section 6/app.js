413d79fc50af680182c8e019a31126607efe08e2
Christian Serrano <nano561@icloud.com>
2019-08-15 23:33:57 -0400

    added event listeners and ctrlAddItem function
    added TODO List
    Bug: when class: add__btn (or btn clicked) is called, then followed by keypress 13 (enter). You get two key press instead of one.

--- Section 6/app.js
+++ Section 6/app.js
@@ -14,6 +14,27 @@ var UIController = (function () {
 
 var controller = (function (budgetCtrl, UICtrl) {
 
+    var ctrlAddItem = function () {
+
+        // TODO 1. Get the field input data
+
+        // TODO 2. Add the item to the budget controller
+
+        // TODO 3. Add the item to the UI
+
+        // TODO 4. Calculate the budget
+
+        // TODO 5. Display the budget on the UI
+
+    };
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function (event) {
+        if (event.keyCode === 13 || event.which === 13 /*|| event.key === "Enter"*/) {
+            ctrlAddItem();
+        }
+    });
 
 
 })(budgetController, UIController);
