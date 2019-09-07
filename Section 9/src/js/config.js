d3cc3b47f83ecee6ac2b83fc4a3fde3b8f38a853
Christian Serrano <nano561@icloud.com>
2019-09-07 17:15:15 -0400

    Updated Config.js

---
+++ Section 9/src/js/config.js
@@ -0,0 +1,3 @@
+export const proxy = ''; // https://cors-anywhere.herokuapp.com/  if Access-Control-Allow-Origin is error
+export const key = '11e0fd5df169ec583ec0d0e154028006';
+//Never store key on client side, always in your server
